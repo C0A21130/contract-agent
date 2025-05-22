77552bb101077ff82dadb2f2a65f892523ad2681
YukiYasuno <85671824+C0A21130@users.noreply.github.com>
2025-05-22 18:54:06 +0900

    Add SsdlabToken contract and associated tools for minting and fetching NFTs

--- tools/tools.py
+++ tools/tools.py
@@ -1,5 +1,6 @@
+from typing import Annotated, List
 from langchain_core.tools import tool
-from tools.contract import Contract
+from tools.contract import Contract, Token
 
 def get_tools(rpc_url: str, contract_address: str, private_key: str):
     
@@ -10,15 +11,15 @@ def get_tools(rpc_url: str, contract_address: str, private_key: str):
         private_key=private_key,
     )
 
-    # @tool
-    # def add(x: int, y: int) -> int:
-    #     """Add two numbers together."""
-    #     return x + y
+    @tool
+    def add(x: int, y: int) -> int:
+        """Add two numbers together."""
+        return x + y
 
-    # @tool
-    # def sub(x: int, y: int) -> int:
-    #     """Substract two numbers together."""
-    #     return x - y
+    @tool
+    def sub(x: int, y: int) -> int:
+        """Substract two numbers together."""
+        return x - y
     
     @tool
     def set_name(name: str) -> str:
@@ -44,4 +45,46 @@ def get_tools(rpc_url: str, contract_address: str, private_key: str):
         name = contract.get_name()
         return name
 
-    return [set_name, get_name]
+    @tool
+    def put_token(
+        to_address: Annotated[str, "The address to mint the token to"],
+        token_name: Annotated[str, "The name of the token to mint"],
+    ) -> int:
+        """
+        This tool is called smart contract.
+        Mint a token to the specified address.
+        Returns:
+            int: The token ID of the minted NFT.
+        """
+        from_address = contract.get_address()
+        token_id = contract.mint(from_address, token_name)
+        if token_id == -1:
+            return -1
+        else:
+            contract.transfer(from_address, to_address, token_id)
+            return token_id
+
+    @tool
+    def fetch_token(
+        address: Annotated[str, "The address to fetch tokens from"],
+    ) -> List[Token]:
+        """
+        This tool is called smart contract.
+        Fetch all NFT(Non Fungible Token) for the address.
+        Returns:
+            List[Token]:
+            Token: {
+                "from_address": str,
+                "to_address": str,
+                "token_id": int,
+                "token_name": str,
+            }
+        1. from_address: The address to which the token to transfer.
+        2. to_address: The address where you received the NFT.
+        3. token_id: The ID of the token.
+        4. token_name: The name of the token.
+        """
+        tokens = contract.fetch_tokens()
+        return tokens
+
+    return [add, sub, put_token, fetch_token]
